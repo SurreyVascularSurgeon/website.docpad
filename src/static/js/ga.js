d315ffd3e29d18999c52e3f2658050d551206696
Neville Dastur <neville@dastur.me.uk>
2015-10-23 22:53:38 +0100

    link to nhs telephone number and add in some google analytics

--- src/static/js/ga.js
+++ src/static/js/ga.js
@@ -5,3 +5,18 @@ m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
 
 ga('create', 'UA-38705943-3', 'auto');
 ga('send', 'pageview');
+
+
+//- Function that tracks a click on an outbound link in Google Analytics.
+//- This function takes a valid URL string as an argument, and uses that URL string
+//- as the event label.
+var trackOutboundLink = function(url) {
+   ga('send', 'event', 'outbound', 'click', url, {'hitCallback':
+     function () {
+     document.location = url;
+     }
+   });
+}
+
+//- You'll also need to add (or modify) the onclick attribute to your links. Use this example as a model for your own links:
+//- <a href="http://www.example.com" onclick="trackOutboundLink('http://www.example.com'); return false;">Check out example.com</a>
